3821e8d54c75aa8cbfa494d889fc4749ec04fd85
anugrahsinghal <anugrahsinghal1@gmail.com>
2022-11-07 11:27:28 +0000

    fix: compilation errors

--- src/main/java/com/cogito/hackerearth/service/MessageReceiver.java
+++ src/main/java/com/cogito/hackerearth/service/MessageReceiver.java
@@ -1,8 +1,11 @@
 package com.cogito.hackerearth.service;
 
 import com.cogito.hackerearth.service.internal.AudioEvent;
+import io.awspring.cloud.messaging.listener.SqsMessageDeletionPolicy;
+import io.awspring.cloud.messaging.listener.annotation.SqsListener;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -12,18 +15,12 @@ public class MessageReceiver {
 
     private final SpeechToText speechToText;
 
-    @Slf4j
-    @Service
-    public class MessageReceiver {
+    @SqsListener(value = "testObjectQueue", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
+    public void receiveMessage(final AudioEvent message,
+                               @Header("SenderId") String senderId) {
+        log.info("message received {} {}", senderId, message);
 
-        @SqsListener(value = "testObjectQueue", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
-        public void receiveMessage(final AudioEvent message,
-                                   @Header("SenderId") String senderId) {
-            log.info("message received {} {}", senderId, message);
-
-            speechToText.convert(message.getInput());
-
-
-        }
+        speechToText.convert(message.getInput());
     }
+
 }
