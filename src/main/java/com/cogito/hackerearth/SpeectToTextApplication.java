91838a30b0bbfd76b0eed98e34b59855d0372b41
Anugrah Singhal <anugrahsinghal1@gmail.com>
2022-11-07 16:49:42 +0530

    feat: initial project skeleton

---
+++ src/main/java/com/cogito/hackerearth/SpeectToTextApplication.java
@@ -0,0 +1,13 @@
+package com.cogito.hackerearth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeectToTextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeectToTextApplication.class, args);
+    }
+
+}
